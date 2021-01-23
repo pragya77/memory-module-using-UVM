edbcbd2a7457b47eae9c8d511d5b45009b30a7de
Pragya Pusp <48910334+pragya77@users.noreply.github.com>
2021-01-23 14:20:47 -0500

    Update tb.sv

--- tb.sv
+++ tb.sv
@@ -15,17 +15,8 @@
 	`include "env.sv"
 	`include "test.sv"
 
+
  	`timescale 1ns/100ps 
-
-interface intf(input clk, input reset1);
-
-	logic rd_wr1;
-  logic [7:0] wr_data1;
-  logic [7:0] rd_data1;
-  logic [7:0] addr1;
-  
- endinterface
-
 
  module tb;
    
@@ -39,8 +30,7 @@ interface intf(input clk, input reset1);
     always #10 clk = ~clk;
 
 	initial begin
-		uvm_config_db #(virtual intf)::set(uvm_root::get(),"*","vif",vif);
-
+      uvm_config_db #(virtual intf)::set(uvm_root::get(),"*","vif",vif);
       	
       	$dumpfile("dump.vcd");
         $dumpvars;
@@ -48,6 +38,7 @@ interface intf(input clk, input reset1);
 	end
 
     initial begin
+      
       rst = 1;
       #20
       rst = 0;
